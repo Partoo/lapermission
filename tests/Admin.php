4746f0123e543e28341fe7a064ba2bdf631879a9
Partoo Hwong <partoo@163.com>
2019-04-28 01:45:01 +0800

    init

---
+++ tests/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Partoo\Permission\Test;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Partoo\Permission\Traits\HasRoles;
+
+class Admin extends Model implements AuthorizableContract, AuthenticatableContract
+{
+    use HasRoles, Authorizable, Authenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['email'];
+
+    public $timestamps = false;
+
+    protected $table = 'admins';
+}
---
+++ tests/SoftDeletingUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Partoo\Permission\Test;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SoftDeletingUser extends User
+{
+    use SoftDeletes;
+
+    protected $guard_name = 'web';
+}
